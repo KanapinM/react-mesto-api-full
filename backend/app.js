506828fef662fbaf735102fcfd147cebf3266ff1
Kanapin Marat <kanapinmarat@yandex.ru>
2023-02-12 23:08:15 +0600

    add CORS

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,32 @@ const NotFoundError = require('./errors/NotFoundError');
 
 const { PORT = 3000 } = process.env;
 const env = 'mongodb://localhost:27017/mestodb';
+
+const allowedCors = [
+  'https://mkmesto.nomoredomains.work',
+  'http://mkmesto.nomoredomains.work',
+  'localhost:3000',
+];
+
 const app = express();
+
+// eslint-disable-next-line prefer-arrow-callback
+app.use(function (req, res, next) {
+  const { method } = req; // Сохраняем источник запроса в переменную origin
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+  // проверяем, что источник запроса есть среди разрешённых
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  next();
+});
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
