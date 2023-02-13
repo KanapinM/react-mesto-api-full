65c6a62f51f37c4e69241e59cff1b0c783d77562
Kanapin Marat <kanapinmarat@yandex.ru>
2023-02-13 23:00:42 +0600

    fix auth

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -67,7 +67,7 @@ module.exports.login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: '7d' });
-      return res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: false })
+      return res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: false, domain: 'http://mkmesto.nomoredomains.work' })
         .send({ token });
     })
     .catch(next);
